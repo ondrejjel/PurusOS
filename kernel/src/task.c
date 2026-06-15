eb2afdb2078d4006be1b7e275f2e644eb6759873
ondrej jelinek <ondrejj.dev@gmail.com>
2026-06-15 21:15:49 +0200

    added safety check

--- kernel/src/task.c
+++ kernel/src/task.c
@@ -5,11 +5,11 @@
 
 
 
-typedef enum taskState_t{       // tasks will have one of those states
+typedef enum TaskState_t{       // tasks will have one of those states
     RUNNING,                    // task is running when scheduler swiches to it
     READY,                      // task is ready after yielding back to scheduler
     BLOCKED                     // task is blocked when waiting for external condition or when fault is detected
-}taskState_t;
+}TaskState_t;
 
 typedef struct TCB_t {
     void* stackPtr;             // CURRENT stack pointer
@@ -22,14 +22,17 @@ typedef struct TCB_t {
 
 
     uint16_t id;                // Task ID
-    taskState_t state;          // tasks current state
+    TaskState_t state;          // tasks current state
 } TCB_t;
 
 
 static uint16_t taskId = 0;
 
+__attribute__((nonnull(1)))
 TCB_t x_pu_create_task(void (*task)(void* arg),void* arguments, size_t stackSize){
-    TCB_t tcb = {NULL, NULL, NULL, NULL, NULL, NULL, NULL}; //reset everything
+    TCB_t tcb = {0}; //reset everything
+
+
     tcb.id = taskId;    //  set the id
     taskId++;           //  increment global id counter
 
@@ -38,8 +41,12 @@ TCB_t x_pu_create_task(void (*task)(void* arg),void* arguments, size_t stackSize
     Arena_t taskAdresses = x_pu_alloc_arena(stackSize); // get adresses
     tcb.stackBottom = taskAdresses.stackBottom;         // assign bottom adress
     tcb.stackTop = taskAdresses.stackTop;               // assign top adress
-
     tcb.stackPtr = taskAdresses.stackTop;               // stack pointer has to be on upper bound because stack grows downward
+
+    if (tcb.stackBottom == NULL || tcb.stackTop == NULL || stackSize == 0) { // null ptr safety check
+        tcb.id = 0x1111;    // set id to task creation error value
+        return tcb;         // return invalid task block
+    }
 
     tcb.task = task;        // pass function pointer
     tcb.arg = arguments;    // pass arguments
