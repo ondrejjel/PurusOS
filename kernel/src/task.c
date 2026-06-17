efcb3a60b417ea527c3f33da2503f071d8fa4557
ondrej jelinek <ondrejj.dev@gmail.com>
2026-06-17 20:53:53 +0200

    fix logic bug in task creation, add default INVALID state, add errorCode field to TCB to provide better understanding of fault and keep other fields intact

--- kernel/src/task.c
+++ kernel/src/task.c
@@ -6,6 +6,7 @@
 
 
 typedef enum TaskState_t{       // tasks will have one of those states
+    INVALID,                    // task is invalid when any fault happens or if there is a initialisation issue
     RUNNING,                    // task is running when scheduler swiches to it
     READY,                      // task is ready after yielding back to scheduler
     BLOCKED                     // task is blocked when waiting for external condition or when fault is detected
@@ -15,12 +16,12 @@ typedef struct TCB_t {
     void* stackPtr;             // CURRENT stack pointer
 
     void (*task)(void *arg);    // Pointer to the task function
-    void* arg;                  // Argument passed to the task function
+    void* arg;                  // Arguments passed to the task function
 
     void* stackBottom;          // bottom stack boundary
     void* stackTop;             // top stack boundary
 
-
+    uint32_t errorCode;         // task error field
     uint16_t id;                // Task ID
     TaskState_t state;          // tasks current state
 } TCB_t;
@@ -30,13 +31,7 @@ static uint16_t taskId = 0;
 
 __attribute__((nonnull(1)))
 TCB_t x_pu_create_task(void (*task)(void* arg),void* arguments, size_t stackSize){
-    TCB_t tcb = {0}; //reset everything
-
-
-    tcb.id = taskId;    //  set the id
-    taskId++;           //  increment global id counter
-
-    tcb.state = READY;  // default state
+    TCB_t tcb = {0}; // reset everything
 
     Arena_t taskAdresses = x_pu_alloc_arena(stackSize); // get adresses
     tcb.stackBottom = taskAdresses.stackBottom;         // assign bottom adress
@@ -44,9 +39,16 @@ TCB_t x_pu_create_task(void (*task)(void* arg),void* arguments, size_t stackSize
     tcb.stackPtr = taskAdresses.stackTop;               // stack pointer has to be on upper bound because stack grows downward
 
     if (tcb.stackBottom == NULL || tcb.stackTop == NULL || stackSize == 0) { // null ptr safety check
-        tcb.id = 0x1111;    // set id to task creation error value
-        return tcb;         // return invalid task block
+        //todo: change hardcoded value to enum field
+        tcb.errorCode = 0xFFFFAAAA; // set error code to signalise a fault
+        return tcb;                 // return invalid task block
     }
+
+    tcb.id = taskId;    //  set the id
+    taskId++;           //  increment global id counter
+
+    tcb.state = READY;  // default state
+
 
     tcb.task = task;        // pass function pointer
     tcb.arg = arguments;    // pass arguments
