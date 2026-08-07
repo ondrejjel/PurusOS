478f61d1d43f68d7adb42efe87ff0d80e27ac5fa
ondrej jelinek <ondrejj.dev@gmail.com>
2026-08-07 20:47:55 +0200

    Kernel interface: added header for kernel interface

---
+++ kernel/include/kernel_interface.h
@@ -0,0 +1,26 @@
+#ifndef  PU_KERNEL_INTERFACE
+#define PU_KERNEL_INTERFACE
+
+#include <stdint.h>
+#include <allocator.h>
+#include <task.h>
+
+typedef struct
+{
+    uintptr_t (*context_save)(void);
+    void (*context_restore)(uintptr_t context);
+    uintptr_t (*context_create)(MemoryBlock_t *memory, const Task_t *task);
+} KernelInterface_t;
+
+KernelInterface_t activeInterface = {0};
+
+uintptr_t uptr_pu_task_context_save(void);
+
+void v_pu_task_context_restore(uintptr_t context);
+
+uintptr_t uptr_pu_task_context_create(MemoryBlock_t memory, const Task_t *task);
+
+
+void v_pu_kernel_use_interface(const KernelInterface_t *interface);
+
+#endif // PU_KERNEL_INTERFACE
