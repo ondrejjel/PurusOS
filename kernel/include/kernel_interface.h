e85e178fad664f0deb6a9681b8b2fa2b99f3fab6
ondrej jelinek <ondrejj.dev@gmail.com>
2026-08-11 19:26:55 +0200

    Kernel interface: removed public access to activeInterface

--- kernel/include/kernel_interface.h
+++ kernel/include/kernel_interface.h
@@ -24,8 +24,6 @@ typedef struct
     uintptr_t (*context_create)(MemoryBlock_t memory, const Task_t *task);
 } KernelInterface_t;
 
-extern KernelInterface_t activeInterface;
-
 uintptr_t uptr_pu_task_context_save(void);
 
 void v_pu_task_context_restore(uintptr_t context);
