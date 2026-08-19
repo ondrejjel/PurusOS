4b4fc6f59fb4f80538ac34427c339028c1d81a95
ondrej jelinek <ondrejj.dev@gmail.com>
2026-08-19 19:53:44 +0200

    Orchestration layer: minor rename + initial context restore added

--- kernel/src/PurusOS.c
+++ kernel/src/PurusOS.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #include <stdint.h>
 #include <allocator.h>
 #include <fault_code.h>
@@ -16,6 +17,8 @@ void startKernel(void)
     if (s_pu_get_loaded_task_count() != 0)
     {
         v_pu_set_initial_task();
+        uintptr_t initialContext = uptr_pu_get_active_task_context();
+        v_pu_task_context_restore(initialContext);
     }
     else
     {
@@ -23,11 +26,17 @@ void startKernel(void)
     }
 }
 
+void newTask(void (*task)(void *arg), void *arguments, size_t stackSize)
+{
+    TCB_t tcb = x_pu_create_task(task, arguments, stackSize);
+    v_pu_load_task(tcb);
+}
+
 void taskYield(void)
 {
-    uintptr_t saved_context = uptr_pu_task_context_save();
-    v_pu_update_active_task_context(saved_context);
+    uintptr_t savedContext = uptr_pu_task_context_save();
+    v_pu_update_active_task_context(savedContext);
     v_pu_choose_next_task();
-    uintptr_t new_context = uptr_pu_get_active_task_context();
-    v_pu_task_context_restore(new_context);
+    uintptr_t newContext = uptr_pu_get_active_task_context();
+    v_pu_task_context_restore(newContext);
 }
